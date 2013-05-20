7ddd1d327e1342b7609ec62e5df7062dc46409f9
Moerin <trash.sebatien@gmail.com>
2013-05-20 22:00:44 +0200

    DAO Creation

--- src/java/fr/urlshortener/dao/DAO.java
+++ src/java/fr/urlshortener/dao/DAO.java
@@ -4,19 +4,34 @@
  */
 package fr.urlshortener.dao;
 
+import fr.urlshortener.database.ConnectNeo4j;
+
 /**
  *
  * @author Seb
  */
 public abstract class DAO<T> {
     
+    // TODO : se renseigner le chemin relatif sur un serveur JEE
+    protected ConnectNeo4j connect;
+    // TODO : trouver une moyen d'appeler une instance de Neo4j
+    public DAO(ConnectNeo4j conn){
+        this.connect = conn;
+    }
     
 	/**
 	 * Permet de récupérer un objet via son ID
 	 * @param id
 	 * @return
 	 */
-	public abstract T find(long id);
+	public abstract T findID(long id);
+        
+        /**
+         * Permet de récupérer un objet via sa propriete
+         * @param property
+         * @return 
+         */
+        public abstract T findValue(String property);
 	
 	/**
 	 * Permet de créer une entrée dans la base de données
